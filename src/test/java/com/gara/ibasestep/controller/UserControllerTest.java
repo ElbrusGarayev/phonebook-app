73c77702466f7ea4c476dfa457cd9d4a5a80abf0
ElbrusGarayev <ekberli.elbrus@gmail.com>
2022-03-04 00:09:39 +0400

    some updates

---
+++ src/test/java/com/gara/ibasestep/controller/UserControllerTest.java
@@ -0,0 +1,143 @@
+package com.gara.ibasestep.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gara.ibasestep.model.request.AddUserRequest;
+import com.gara.ibasestep.model.request.UpdateUserRequest;
+import com.gara.ibasestep.model.response.UserOperationResponse;
+import com.gara.ibasestep.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+
+import static com.gara.ibasestep.enums.OperationStatus.SUCCESS;
+import static com.gara.ibasestep.enums.OperationType.ADD;
+import static com.gara.ibasestep.enums.OperationType.DELETE;
+import static com.gara.ibasestep.enums.OperationType.EDIT;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    private UserOperationResponse userOperationResponse;
+    private UpdateUserRequest updateUserRequest;
+    private AddUserRequest addUserRequest;
+
+    @BeforeEach
+    void setUp() {
+        setUpUserOperationResponse();
+        setUpUpdateUserRequest();
+        setUpAddUserRequest();
+    }
+
+    @Test
+    void testGetUsers() throws Exception {
+        // arrange
+        when(userService.getUsers()).thenReturn(new ArrayList<>());
+
+        // act & assert
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/user/api");
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testAddUser() throws Exception {
+        // arrange
+        userOperationResponse.setOperationType(ADD);
+        when(userService.addUser(any())).thenReturn(userOperationResponse);
+
+        // act & assert
+        String content = (new ObjectMapper()).writeValueAsString(addUserRequest);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/user/api")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"userId\":123,\"operationType\":\"ADD\",\"operationStatus\":\"SUCCESS\"}"));
+    }
+
+    @Test
+    void testDeleteUser() throws Exception {
+        // arrange
+        userOperationResponse.setOperationType(DELETE);
+        when(userService.deleteUser(anyInt())).thenReturn(userOperationResponse);
+
+        // act & assert
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/user/api/{userId}", 123);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"userId\":123,\"operationType\":\"DELETE\",\"operationStatus\":\"SUCCESS\"}"));
+    }
+
+    @Test
+    void testUpdateUser() throws Exception {
+        // arrange
+        userOperationResponse.setOperationType(EDIT);
+        when(userService.updateUser(anyInt(), any())).thenReturn(userOperationResponse);
+
+        // act & assert
+        String content = (new ObjectMapper()).writeValueAsString(updateUserRequest);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/user/api/{userId}", 123)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(userController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("{\"userId\":123,\"operationType\":\"EDIT\",\"operationStatus\":\"SUCCESS\"}"));
+    }
+
+    private void setUpUserOperationResponse() {
+        userOperationResponse = UserOperationResponse.builder()
+                .operationStatus(SUCCESS)
+                .userId(123)
+                .build();
+    }
+
+    private void setUpUpdateUserRequest() {
+        updateUserRequest = UpdateUserRequest.builder()
+                .phone("123456")
+                .name("Name")
+                .build();
+    }
+
+    private void setUpAddUserRequest() {
+        addUserRequest = AddUserRequest.builder()
+                .phone("123456")
+                .name("Name")
+                .build();
+    }
+
+}
+
--- src/test/java/com/gara/ibasestep/service/impl/UserServiceImplTest.java
+++ src/test/java/com/gara/ibasestep/service/impl/UserServiceImplTest.java
@@ -1,165 +1,159 @@
-//package com.gara.ibasestep.service.impl;
-//
-//import com.gara.ibasestep.dao.entity.User;
-//import com.gara.ibasestep.dao.repository.UserRepository;
-//import com.gara.ibasestep.model.request.AddUserRequest;
-//import com.gara.ibasestep.model.request.UpdateUserRequest;
-//import com.gara.ibasestep.model.response.UserOperationResponse;
-//import com.gara.ibasestep.model.response.UserResponse;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static com.gara.ibasestep.enums.OperationStatus.FAILED;
-//import static com.gara.ibasestep.enums.OperationStatus.SUCCESS;
-//import static com.gara.ibasestep.enums.OperationType.ADD;
-//import static com.gara.ibasestep.enums.OperationType.DELETE;
-//import static com.gara.ibasestep.enums.OperationType.EDIT;
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.Mockito.any;
-//import static org.mockito.Mockito.doNothing;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//@ContextConfiguration(classes = {UserServiceImpl.class})
-//@ExtendWith(SpringExtension.class)
-//class UserServiceImplTest {
-//
-//    private static final int ID = 1;
-//    private static final int SIZE = 1;
-//    private static final String NAME = "John Doe";
-//    private static final String PHONE = "4105551213";
-//
-//    @MockBean
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private UserServiceImpl userServiceImpl;
-//
-//    private User user;
-//    private UpdateUserRequest updateUserRequest;
-//
-//    @BeforeEach
-//    void setUp() {
-//        setUpUser();
-//        setUpUpdateUserRequest();
-//    }
-//
-//    @Test
-//    void getUsersThenSuccessResult() {
-//        // arrange
-//        when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
-//
-//        // act
-//        List<UserResponse> actualUsers = userServiceImpl.getUsers();
-//
-//        // assert
-//        assertThat(actualUsers.size()).isSameAs(SIZE);
-//        UserResponse getResult = actualUsers.get(0);
-//        assertThat(getResult.getName()).isSameAs(NAME);
-//        assertThat(getResult.getUserId()).isSameAs(ID);
-//        assertThat(getResult.getPhone()).isSameAs(PHONE);
-//        verify(userRepository).findAll();
-//    }
-//
-//    @Test
-//    void addUserThenSuccessResult() {
-//        // arrange
-//        when(userRepository.save(any())).thenReturn(user);
-//
-//        // act
-//        UserOperationResponse actualAddUserResult = userServiceImpl.addUser(new AddUserRequest(NAME, PHONE));
-//
-//        // assert
-//        assertThat(actualAddUserResult.getOperationStatus()).isSameAs(SUCCESS);
-//        assertThat(actualAddUserResult.getUserId()).isSameAs(ID);
-//        assertThat(actualAddUserResult.getOperationType()).isSameAs(ADD);
-//        verify(userRepository).save(any());
-//        assertThat(userServiceImpl.getUsers().isEmpty()).isTrue();
-//    }
-//
-//    @Test
-//    void addUserThenFailResult() {
-//        // arrange
-//        when(userRepository.save(any())).thenReturn(user);
-//
-//        // act
-//        UserOperationResponse actualAddUserResult = userServiceImpl.addUser(null);
-//
-//        // assert
-//        assertThat(actualAddUserResult.getOperationStatus()).isSameAs(FAILED);
-//        assertThat(actualAddUserResult.getOperationType()).isSameAs(ADD);
-//    }
-//
-//    @Test
-//    void updateUserThenSuccessResult() {
-//        // arrange
-//        when(userRepository.save(any())).thenReturn(user);
-//        when(userRepository.findById(any())).thenReturn(Optional.of(user));
-//
-//        // act
-//        UserOperationResponse actualUpdateUserResult = userServiceImpl.updateUser(ID, updateUserRequest);
-//
-//        // assert
-//        assertThat(actualUpdateUserResult.getOperationStatus()).isSameAs(SUCCESS);
-//        assertThat(actualUpdateUserResult.getUserId()).isSameAs(ID);
-//        assertThat(actualUpdateUserResult.getOperationType()).isSameAs(EDIT);
-//        verify(userRepository).findById(any());
-//        verify(userRepository).save(any());
-//    }
-//
-//    @Test
-//    void updateUserThenFailResult() {
-//        // arrange
-//        when(userRepository.save(any())).thenReturn(user);
-//        when(userRepository.findById(any())).thenReturn(Optional.empty());
-//
-//        // act
-//        UserOperationResponse actualUpdateUserResult = userServiceImpl.updateUser(ID, updateUserRequest);
-//
-//        // assert
-//        assertThat(actualUpdateUserResult.getOperationStatus()).isSameAs(FAILED);
-//        assertThat(actualUpdateUserResult.getOperationType()).isSameAs(EDIT);
-//        verify(userRepository).findById(any());
-//    }
-//
-//    @Test
-//    void deleteUserThenSuccessResult() {
-//        // arrange
-//        doNothing().when(userRepository).deleteById(any());
-//
-//        // act
-//        UserOperationResponse actualDeleteUserResult = userServiceImpl.deleteUser(ID);
-//
-//        // assert
-//        assertThat(actualDeleteUserResult.getOperationStatus()).isSameAs(SUCCESS);
-//        assertThat(actualDeleteUserResult.getUserId()).isSameAs(ID);
-//        assertThat(actualDeleteUserResult.getOperationType()).isSameAs(DELETE);
-//        verify(userRepository).deleteById(any());
-//    }
-//
-//    private void setUpUser() {
-//        user = User.builder()
-//                .id(ID)
-//                .name(NAME)
-//                .phone(PHONE)
-//                .build();
-//    }
-//
-//    private void setUpUpdateUserRequest() {
-//        updateUserRequest = UpdateUserRequest.builder()
-//                .name(NAME)
-//                .phone(PHONE)
-//                .build();
-//    }
-//
-//}
-//
+package com.gara.ibasestep.service.impl;
+
+import com.gara.ibasestep.dao.entity.User;
+import com.gara.ibasestep.dao.repository.UserRepository;
+import com.gara.ibasestep.model.request.AddUserRequest;
+import com.gara.ibasestep.model.request.UpdateUserRequest;
+import com.gara.ibasestep.model.response.UserOperationResponse;
+import com.gara.ibasestep.model.response.UserResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static com.gara.ibasestep.enums.OperationStatus.FAILED;
+import static com.gara.ibasestep.enums.OperationStatus.SUCCESS;
+import static com.gara.ibasestep.enums.OperationType.ADD;
+import static com.gara.ibasestep.enums.OperationType.DELETE;
+import static com.gara.ibasestep.enums.OperationType.EDIT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    private static final int ID = 1;
+    private static final int SIZE = 1;
+    private static final String NAME = "John Doe";
+    private static final String PHONE = "4105551213";
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
+
+    private User user;
+    private UpdateUserRequest updateUserRequest;
+
+    @BeforeEach
+    void setUp() {
+        setUpUser();
+        setUpUpdateUserRequest();
+    }
+
+    @Test
+    void getUsersThenSuccessResult() {
+        // arrange
+        when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
+
+        // act
+        List<UserResponse> actualUsers = userServiceImpl.getUsers();
+
+        // assert
+        assertThat(actualUsers.size()).isSameAs(SIZE);
+        UserResponse getResult = actualUsers.get(0);
+        assertThat(getResult.getName()).isSameAs(NAME);
+        assertThat(getResult.getUserId()).isSameAs(ID);
+        assertThat(getResult.getPhone()).isSameAs(PHONE);
+        verify(userRepository).findAll();
+    }
+
+    @Test
+    void addUserThenSuccessResult() {
+        // arrange
+        when(userRepository.save(any())).thenReturn(user);
+
+        // act
+        UserOperationResponse actualAddUserResult = userServiceImpl.addUser(new AddUserRequest(NAME, PHONE));
+
+        // assert
+        assertThat(actualAddUserResult.getOperationStatus()).isSameAs(SUCCESS);
+        assertThat(actualAddUserResult.getUserId()).isSameAs(ID);
+        assertThat(actualAddUserResult.getOperationType()).isSameAs(ADD);
+        verify(userRepository).save(any());
+        assertThat(userServiceImpl.getUsers().isEmpty()).isTrue();
+    }
+
+    @Test
+    void addUserThenFailResult() {
+        // act
+        UserOperationResponse actualAddUserResult = userServiceImpl.addUser(null);
+
+        // assert
+        assertThat(actualAddUserResult.getOperationStatus()).isSameAs(FAILED);
+        assertThat(actualAddUserResult.getOperationType()).isSameAs(ADD);
+    }
+
+    @Test
+    void updateUserThenSuccessResult() {
+        // arrange
+        when(userRepository.save(any())).thenReturn(user);
+        when(userRepository.findById(any())).thenReturn(Optional.of(user));
+
+        // act
+        UserOperationResponse actualUpdateUserResult = userServiceImpl.updateUser(ID, updateUserRequest);
+
+        // assert
+        assertThat(actualUpdateUserResult.getOperationStatus()).isSameAs(SUCCESS);
+        assertThat(actualUpdateUserResult.getUserId()).isSameAs(ID);
+        assertThat(actualUpdateUserResult.getOperationType()).isSameAs(EDIT);
+        verify(userRepository).findById(any());
+        verify(userRepository).save(any());
+    }
+
+    @Test
+    void updateUserThenFailResult() {
+        // arrange
+        when(userRepository.findById(any())).thenReturn(Optional.empty());
+
+        // act
+        UserOperationResponse actualUpdateUserResult = userServiceImpl.updateUser(ID, updateUserRequest);
+
+        // assert
+        assertThat(actualUpdateUserResult.getOperationStatus()).isSameAs(FAILED);
+        assertThat(actualUpdateUserResult.getOperationType()).isSameAs(EDIT);
+        verify(userRepository).findById(any());
+    }
+
+    @Test
+    void deleteUserThenSuccessResult() {
+        // arrange
+        doNothing().when(userRepository).deleteById(any());
+
+        // act
+        UserOperationResponse actualDeleteUserResult = userServiceImpl.deleteUser(ID);
+
+        // assert
+        assertThat(actualDeleteUserResult.getOperationStatus()).isSameAs(SUCCESS);
+        assertThat(actualDeleteUserResult.getUserId()).isSameAs(ID);
+        assertThat(actualDeleteUserResult.getOperationType()).isSameAs(DELETE);
+        verify(userRepository).deleteById(any());
+    }
+
+    private void setUpUser() {
+        user = User.builder()
+                .id(ID)
+                .name(NAME)
+                .phone(PHONE)
+                .build();
+    }
+
+    private void setUpUpdateUserRequest() {
+        updateUserRequest = UpdateUserRequest.builder()
+                .name(NAME)
+                .phone(PHONE)
+                .build();
+    }
+
+}
+
